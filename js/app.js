a67fe3ada7ac68fc3bfe0297da495d0325e5413e
Pham Xuan Viet Cuong <pxvcuong2347@gmail.com>
2022-11-06 13:36:07 +0700

    Countdown end

--- js/app.js
+++ js/app.js
@@ -1,45 +1,47 @@
 const countdown = () => {
-	let endDay = new Date('February 20, 2022 07:05:54').getTime();
+	let endDay = new Date("February 20, 2022 07:05:54").getTime();
 	let now = new Date().getTime();
-	
-	if (now >= endDay) {
-		changeDate = endDay + 1000 * 60 * 60 * 24 * 50;
-		endDay = new Date(changeDate).getTime();
-	}
-	
+
 	let difference = endDay - now;
 
 	let sec = 1000;
 	let min = sec * 60;
 	let hour = min * 60;
 	let day = hour * 24;
-	
 
 	let dayDisplay = Math.floor(difference / day);
 	let hourDisplay = Math.floor((difference % day) / hour);
 	let minDisplay = Math.floor((difference % hour) / min);
 	let secDisplay = Math.floor((difference % min) / sec);
 
-
 	if (dayDisplay <= 9 && dayDisplay >= 0) {
-		dayDisplay = '0' + dayDisplay;
+		dayDisplay = "0" + dayDisplay;
 	}
 
 	if (hourDisplay <= 9 && hourDisplay >= 0) {
-		hourDisplay = '0' + secDisplay;
+		hourDisplay = "0" + secDisplay;
 	}
 
 	if (minDisplay <= 9 && minDisplay >= 0) {
-		minDisplay = '0' + minDisplay;
+		minDisplay = "0" + minDisplay;
 	}
 
 	if (secDisplay <= 9 && secDisplay >= 0) {
-		secDisplay = '0' + secDisplay;
+		secDisplay = "0" + secDisplay;
 	}
 
-	document.getElementById('day-timer').textContent = dayDisplay;
-	document.getElementById('hour-timer').textContent = hourDisplay;
-	document.getElementById('minute-timer').textContent = minDisplay;
-	document.getElementById('second-timer').textContent = secDisplay;
+	document.getElementById("day-timer").textContent = dayDisplay;
+	document.getElementById("hour-timer").textContent = hourDisplay;
+	document.getElementById("minute-timer").textContent = minDisplay;
+	document.getElementById("second-timer").textContent = secDisplay;
+
+	if (difference <= 0) {
+		clearInterval(countdownInterval);
+		document.getElementById("day-timer").textContent = "00";
+		document.getElementById("hour-timer").textContent = "00";
+		document.getElementById("minute-timer").textContent = "00";
+		document.getElementById("second-timer").textContent = "00";
+	}
 };
-setInterval(() => countdown(), 1000);
+
+let countdownInterval = setInterval(countdown, 1000);
